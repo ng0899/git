63fab4439234e68044d091d2ecee07ba763a23c9
Develop <test@example.com>
2020-09-14 19:21:49 +0300

    add title var

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php
+
+$title = 'My Page';
+
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title><?=$title;?></title>
 </head>
 <body>
     <h1>Title</h1>
 </body>
 </html>
+
